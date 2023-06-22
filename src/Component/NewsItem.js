079907584a648e0527d92e439f9bc11468f9ff03
vivek26anand <vivek26anand.va@gmail.com>
2023-06-23 00:22:42 +0530

    Props Fix

--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react'
 export   class NewsItem extends Component {
 
   render() {
-   let {title,description,imageUrl,url} = this.props;
+   let {title,description,img,newsUrl} = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-  <img src = {imageUrl}
+  <img src = {img}
    className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
-    <a href={url} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+    <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
   </div>
 </div>
       </div>
